357ecf18261c99514bc80d85809d35f03205c0a7
Aryan230207 <aryan.parmar132191@marwadiuniversity.ac.in>
2026-07-28 18:25:32 +0530

    Add files via upload

---
+++ Practical 2/Practical 2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+interface IPayroll
+{
+    void CalculateSalary();
+    void DisplaySalary();
+}
+class Employee
+{
+    public int EmpId;
+    public string EmpName;
+    public double BasicSalary;
+
+    public Employee(int id, string name, double salary)     // constructor
+    {
+        EmpId = id;
+        EmpName = name;
+        BasicSalary = salary;
+    }
+}
+
+class FullTimeEmployee : Employee, IPayroll
+{
+    double HRA, DA, GrossSalary;                  // variables  
+
+    public FullTimeEmployee(int id, string name, double salary)       // constructor
+        : base(id, name, salary)
+    {
+    }
+
+    public void CalculateSalary()
+    {
+        HRA = BasicSalary * 0.35; // 35%
+        DA = BasicSalary * 0.15;  // 15%
+        GrossSalary = BasicSalary + HRA + DA;
+    }
+
+    public void DisplaySalary()
+    {
+        Console.WriteLine("\n===== FULL TIME EMPLOYEE PAYROLL =====");
+        Console.WriteLine("Employee ID   : " + EmpId);
+        Console.WriteLine("Employee Name : " + EmpName);
+        Console.WriteLine("Basic Salary  : " + BasicSalary);
+        Console.WriteLine("HRA (20%)     : " + HRA);
+        Console.WriteLine("DA (10%)      : " + DA);
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Gross Salary  : " + GrossSalary);
+    }
+}
+
+class PartTimeEmployee : Employee, IPayroll
+{
+    double TotalSalary;                  // variable    
+
+    public PartTimeEmployee(int id, string name, double salary)    // constructor
+        : base(id, name, salary)
+    {
+    }
+
+    public void CalculateSalary()
+    {
+        TotalSalary = BasicSalary;
+    }
+
+    public void DisplaySalary()
+    {
+        Console.WriteLine("\n===== PART TIME EMPLOYEE PAYROLL =====");
+        Console.WriteLine("Employee ID   : " + EmpId);
+        Console.WriteLine("Employee Name : " + EmpName);
+        Console.WriteLine("Basic Salary  : " + BasicSalary);
+        Console.WriteLine("HRA           : 0");
+        Console.WriteLine("DA            : 0");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Total Salary  : " + TotalSalary);
+    }
+}
+
+// Main Class
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("===== EMPLOYEE PAYROLL SYSTEM =====");
+
+        Console.Write("Enter Employee ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Employee Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Basic Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nSelect Employee Type");
+        Console.WriteLine("1. Full Time");
+        Console.WriteLine("2. Part Time");
+        Console.Write("Enter Your Choice: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            FullTimeEmployee emp = new FullTimeEmployee(id, name, salary);
+            emp.CalculateSalary();
+            emp.DisplaySalary();
+        }
+        else if (choice == 2)
+        {
+            PartTimeEmployee emp = new PartTimeEmployee(id, name, salary);
+            emp.CalculateSalary();
+            emp.DisplaySalary();
+        }
+        else
+        {
+            Console.WriteLine("Invalid Choice!");
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
